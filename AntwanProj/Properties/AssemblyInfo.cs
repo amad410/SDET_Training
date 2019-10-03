1f897cbd400798ea5e8ebe91c7365ab71d96934d
Antwan Maddox <antwan.maddox@agilethought.com>
2019-10-03 14:16:22 -0400

    commit

---
+++ AntwanProj/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AntwanProj")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AntwanProj")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9f81bfe9-1957-4290-84bb-01a29d0aed3f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AntwanProj/TestClass1.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDETProj
+{
+    [TestFixture]
+    public class TestClass1
+    {
+        IWebDriver _driver = null;
+        [Test]
+        public void TestMethod()
+        {
+            //test case implementation here
+
+        }
+
+        [SetUp]
+        public void Precondition()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void Postcondition()
+        {
+            _driver.Quit();
+        }
+    }
+}
